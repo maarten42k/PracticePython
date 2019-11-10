22b10a102fd24cc6523b88bc794b97b9d1afc81a
Maarten42k <m.vanbeveren@gmail.com>
2019-11-10 06:43:28 +0100

    Exercise 4 finished

--- Exercise4.py
+++ Exercise4.py
@@ -1,1 +1,8 @@
-print("Hello")
+# Give the divisors of a certain number
+number = int(input("Give me a number: "))
+range = range(1, number+1)
+result = []
+for num in range:
+    if number % num == 0:
+        result.append(num)
+print(result)
